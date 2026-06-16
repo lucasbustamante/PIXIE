d25064efd05afa79d066ca8a7653f3ccb2fe39d1
Lucas Bustamante <lucascostabustamante@gmail.com>
2026-06-16 01:06:09 -0300

    update

--- camxdisplay/camxdisplay.ino
+++ camxdisplay/camxdisplay.ino
@@ -9,6 +9,7 @@
 #define TFT_RST  16
 #define TFT_MOSI 12
 #define TFT_SCLK 14
+#define TFT_BL   0   // Backlight do display
 
 // --- Botões e flash ---
 #define BUTTON_ADC_PIN 2
@@ -37,8 +38,12 @@ Adafruit_ST7735 tft = Adafruit_ST7735(&vspi, TFT_CS, TFT_DC, TFT_RST);
 
 uint16_t lineBuffer[80];
 
+bool sistemaLigado = true;
+bool cameraLigada = false;
+
 unsigned long lastButtonTime = 0;
 const unsigned long debounceDelay = 350;
+const unsigned long tempoSegurarPower = 3000;
 
 enum ButtonType {
   BTN_NONE,
@@ -51,8 +56,25 @@ uint16_t swapRB(uint16_t color) {
   uint8_t r = (color >> 11) & 0x1F;
   uint8_t g = (color >> 5)  & 0x3F;
   uint8_t b = color & 0x1F;
-
   return (b << 11) | (g << 5) | r;
+}
+
+ButtonType lerBotao() {
+  int valor = analogRead(BUTTON_ADC_PIN);
+
+  if (valor < 100) return BTN_FOTO;
+  if (valor > 200 && valor < 900) return BTN_DOWN;
+  if (valor > 900 && valor < 3900) return BTN_UP;
+
+  return BTN_NONE;
+}
+
+void backlightOn() {
+  digitalWrite(TFT_BL, HIGH);
+}
+
+void backlightOff() {
+  digitalWrite(TFT_BL, LOW);
 }
 
 void mostrarMensagem(const char *msg) {
@@ -61,6 +83,19 @@ void mostrarMensagem(const char *msg) {
   tft.setTextSize(1);
   tft.setCursor(5, 6);
   tft.print(msg);
+}
+
+void mostrarContadorPower(const char *acao, int segundos) {
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.setTextSize(1);
+
+  tft.setCursor(8, 35);
+  tft.print(acao);
+
+  tft.setTextSize(3);
+  tft.setCursor(30, 70);
+  tft.print(segundos);
 }
 
 void mostrarFrame(camera_fb_t *fb) {
@@ -80,63 +115,7 @@ void mostrarFrame(camera_fb_t *fb) {
   }
 }
 
-ButtonType lerBotao() {
-  int valor = analogRead(BUTTON_ADC_PIN);
-
-  Serial.print("ADC GPIO2: ");
-  Serial.println(valor);
-
-  // Ajuste essas faixas conforme os valores que aparecerem no Serial Monitor
-
-if (valor < 100) {
-    return BTN_FOTO;
-  }
-
-  if (valor > 200 && valor < 900) {
-    return BTN_DOWN;
-  }
-
-  if (valor > 900 && valor < 3900) {
-    return BTN_UP;
-  }
-
-  return BTN_NONE;
-}
-
-void tirarFotoComFlash() {
-  digitalWrite(FLASH_PIN, HIGH);
-  delay(150);
-
-  camera_fb_t *fb = esp_camera_fb_get();
-
-  if (fb) {
-    mostrarFrame(fb);
-    esp_camera_fb_return(fb);
-    mostrarMensagem("FOTO!");
-  }
-
-  delay(250);
-  digitalWrite(FLASH_PIN, LOW);
-}
-
-void setup() {
-  Serial.begin(115200);
-
-  pinMode(FLASH_PIN, OUTPUT);
-  digitalWrite(FLASH_PIN, LOW);
-
-  pinMode(BUTTON_ADC_PIN, INPUT);
-
-  Serial.printf("PSRAM detectada: %d bytes\n", ESP.getPsramSize());
-
-  vspi.begin(TFT_SCLK, -1, TFT_MOSI, TFT_CS);
-
-  tft.initR(INITR_MINI160x80);
-  tft.setSPISpeed(40000000);
-  tft.setRotation(2);
-  tft.fillScreen(ST77XX_BLACK);
-  tft.invertDisplay(false);
-
+bool iniciarCamera() {
   camera_config_t config;
 
   config.ledc_channel = LEDC_CHANNEL_0;
@@ -172,14 +151,8 @@ void setup() {
   esp_err_t err = esp_camera_init(&config);
 
   if (err != ESP_OK) {
-    Serial.printf("Erro câmera: 0x%x\n", err);
-
-    tft.fillScreen(ST77XX_RED);
-    tft.setCursor(5, 30);
-    tft.setTextColor(ST77XX_WHITE);
-    tft.print("Erro camera");
-
-    return;
+    cameraLigada = false;
+    return false;
   }
 
   sensor_t *s = esp_camera_sensor_get();
@@ -192,25 +165,169 @@ void setup() {
   s->set_brightness(s, 0);
   s->set_contrast(s, 0);
 
+  cameraLigada = true;
+  return true;
+}
+
+void desligarCamera() {
+  if (cameraLigada) {
+    esp_camera_deinit();
+    cameraLigada = false;
+  }
+
+  digitalWrite(PWDN_GPIO_NUM, HIGH);
+}
+
+void ligarSistema() {
+  sistemaLigado = true;
+
+  digitalWrite(PWDN_GPIO_NUM, LOW);
+  delay(100);
+
+  backlightOn();
+
+  tft.initR(INITR_MINI160x80);
+  tft.setSPISpeed(40000000);
+  tft.setRotation(2);
+  tft.invertDisplay(false);
+  tft.enableDisplay(true);
+  tft.fillScreen(ST77XX_BLACK);
+
+  iniciarCamera();
+
+  mostrarMensagem("LIGADO");
+  delay(500);
+}
+
+void desligarSistema() {
+  mostrarMensagem("DESLIGANDO");
+  delay(500);
+
+  digitalWrite(FLASH_PIN, LOW);
+
+  desligarCamera();
+
+  tft.fillScreen(ST77XX_BLACK);
+  tft.enableDisplay(false);
+
+  backlightOff();
+
+  sistemaLigado = false;
+}
+
+void tirarFotoComFlash() {
+  if (!sistemaLigado || !cameraLigada) return;
+
+  digitalWrite(FLASH_PIN, HIGH);
+  delay(150);
+
+  camera_fb_t *fb = esp_camera_fb_get();
+
+  if (fb) {
+    mostrarFrame(fb);
+    esp_camera_fb_return(fb);
+    mostrarMensagem("FOTO!");
+  }
+
+  delay(250);
+  digitalWrite(FLASH_PIN, LOW);
+}
+
+bool verificarPowerSegurado(bool ligar) {
+  if (lerBotao() != BTN_FOTO) return false;
+
+  unsigned long inicio = millis();
+  int ultimoSegundo = -1;
+
+  while (lerBotao() == BTN_FOTO) {
+    unsigned long segurando = millis() - inicio;
+    int restante = 3 - (segurando / 1000);
+
+    if (restante != ultimoSegundo && restante > 0) {
+      ultimoSegundo = restante;
+
+      if (sistemaLigado) {
+        mostrarContadorPower("Desligando", restante);
+      }
+    }
+
+    if (segurando >= tempoSegurarPower) {
+      if (ligar) {
+        ligarSistema();
+      } else {
+        desligarSistema();
+      }
+
+      while (lerBotao() == BTN_FOTO) {
+        delay(50);
+      }
+
+      return true;
+    }
+
+    delay(50);
+  }
+
+  return false;
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(FLASH_PIN, OUTPUT);
+  digitalWrite(FLASH_PIN, LOW);
+
+  pinMode(TFT_BL, OUTPUT);
+  backlightOn();
+
+  pinMode(PWDN_GPIO_NUM, OUTPUT);
+  digitalWrite(PWDN_GPIO_NUM, LOW);
+
+  pinMode(BUTTON_ADC_PIN, INPUT);
+
+  vspi.begin(TFT_SCLK, -1, TFT_MOSI, TFT_CS);
+
+  tft.initR(INITR_MINI160x80);
+  tft.setSPISpeed(40000000);
+  tft.setRotation(2);
+  tft.fillScreen(ST77XX_BLACK);
+  tft.invertDisplay(false);
+  tft.enableDisplay(true);
+
+  iniciarCamera();
+
   tft.fillScreen(ST77XX_BLACK);
 }
 
 void loop() {
   ButtonType botao = lerBotao();
 
-  if (botao != BTN_NONE && millis() - lastButtonTime > debounceDelay) {
+  if (!sistemaLigado) {
+    verificarPowerSegurado(true);
+    delay(100);
+    return;
+  }
+
+  if (botao == BTN_FOTO) {
+    bool desligou = verificarPowerSegurado(false);
+
+    if (!desligou && millis() - lastButtonTime > debounceDelay) {
+      lastButtonTime = millis();
+      tirarFotoComFlash();
+    }
+  } 
+  else if (botao != BTN_NONE && millis() - lastButtonTime > debounceDelay) {
     lastButtonTime = millis();
 
-    if (botao == BTN_FOTO) {
-      tirarFotoComFlash();
-    } 
-    else if (botao == BTN_UP) {
+    if (botao == BTN_UP) {
       mostrarMensagem("UP");
     } 
     else if (botao == BTN_DOWN) {
       mostrarMensagem("DOWN");
     }
   }
+
+  if (!cameraLigada) return;
 
   camera_fb_t *fb = esp_camera_fb_get();
 
